926a659b0df2c0522497e626c235ce0eedecb13e
Cloter Gonçalves Barbosa <cbarbosa@viceri.com.br>
2019-06-11 06:06:25 -0300

    Initial Commit

---
+++ ceep/app/src/main/java/br/com/alura/ceep/ListaNotasActivity.java
@@ -0,0 +1,13 @@
+package br.com.alura.ceep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
